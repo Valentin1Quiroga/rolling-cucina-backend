3c70b59a76e2f6fa197d62b6f76974cd4a0129df
Valentin Quiroga <valegolquiroga@gmail.com>
2024-06-27 17:00:39 -0300

    solucionando problema de cors

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 
 const connectDB = require("./db/db");
 const app = express();
-const usersRoutes = require("./routes/usersRoutes")
-const menuRoutes = require("./routes/menuRoutes")
-const pedidosRoutes= require("./routes/pedidosRoutes")
-require("dotenv").config()
+const usersRoutes = require("./routes/usersRoutes");
+const menuRoutes = require("./routes/menuRoutes");
+const pedidosRoutes = require("./routes/pedidosRoutes");
+require("dotenv").config();
 
-connectDB()
+connectDB();
 const PORT = process.env.PORT;
+
+const corsOptions = {
+  origin: "https://rolling-cucina.netlify.app",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
 
-app.use("/users", usersRoutes)
-app.use("/menu", menuRoutes)
-app.use("/pedidos", pedidosRoutes)
+app.use("/users", usersRoutes);
+app.use("/menu", menuRoutes);
+app.use("/pedidos", pedidosRoutes);
 
-app.listen(4500,()=>{console.log("Server listening on port "+ PORT );})
+app.listen(4500, () => {
+  console.log("Server listening on port " + PORT);
+});
